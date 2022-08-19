d3478d99336cecba55cfc77fb82ead369e932ce9
zzzorigtbaatar <zzzorigtbaatar@gmail.com>
2022-08-18 21:25:04 -0700

    add Thought routes

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,26 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  updateThought,
+  deleteThought,
+  createReaction,
+  deleteReaction,
+} = require("../../controllers/thoughtController.js");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thoughts/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+router.route("/:thoughtId/reactions").post(createReaction);
+
+router.route("/:thoughtId/reactions/:reactionId").delete(deleteReaction);
+
+module.exports = router;
